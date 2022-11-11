15ef01a3fb101c6a04a5ee4b0e45174a5f5525eb
Lerigou <115087583+Lerigou1@users.noreply.github.com>
2022-11-11 11:14:07 -0300

    Add files via upload

--- DesenvolvimentoWEBI-Aquario/aquario.js
+++ DesenvolvimentoWEBI-Aquario/aquario.js
@@ -2,51 +2,29 @@ function calculadora() {
     var altura = parseFloat(document.getElementById("altura").value);
     var largura = parseFloat(document.getElementById("largura").value);
     var comprimento = parseFloat(document.getElementById("comprimento").value);
-    var volume = altura*largura*comprimento
-    var aquecedor = (volume)/1000;
-    var bomba = (aquecedor)*6;
+    var volume = altura*largura*comprimento;
+    let litros = volume/1000;
+    document.getElementById("volume").innerHTML = 'Volume: '+ litros + ' L';
+    let div = Math.ceil(litros/50);
+    let aquecedor = div*50
+    var bomba = (litros)*6;
     var inputs = document.querySelectorAll("input");
+    
+    document.getElementById("aquecedor").innerHTML = 'Aquecedor: '+ aquecedor + ' W';
+    document.getElementById("bomba").innerHTML = 'Bomba: '+ bomba.toFixed(0) + ' L/h';
 
-    inputs.forEach(volume => {
-        if (volume.value <= 0) {
+    inputs.forEach(itens => {
+        if (itens.value <= 0) {
             document.getElementById("volume").innerHTML = 'Valor digitado Inválido';
         }
     });
-    if (volume >= 1000) {
-        var litros = volume/1000;
-        document.getElementById("volume").innerHTML = litros.toFixed(2) + ' L';
+    if (parseFloat(document.getElementById("altura").value) <= 0) {
+        alert("Campo 'Altura' vazio ou Valor Inválido");
     }
-    else if (volume < 1000 && volume > 0) {
-        document.getElementById("volume").innerHTML = volume.toFixed(2) + ' cm³';
+    else if (parseFloat(document.getElementById("largura").value) <= 0) {
+        alert("Campo 'Largura' vazio ou Valor Inválido");
     }
-    else if (volume <= 0) {
-        document.getElementById("volume").innerHTML = 'Valor digitado Inválido';
-    }
-    let v = 0;
-    let a = 50;
-    while (true) {
-        v += 1;
-        if (a+v > aquecedor) {
-            document.getElementById("aquecedor").innerHTML = 'Aquecedor: '+a*v+'W';
-            break;
-        }   
-    }
-    let c = 0;
-    let b = 50;
-    while (true) {
-        c += 1;
-        if (b+c > bomba) {
-            document.getElementById("bomba").innerHTML = 'Bomba: '+b*c+'L/h';
-            break;
-        }
-    }
-    if (parseFloat(altura.value) = 0) {
-        alert("Campo 'Altura' vazio");
-    }
-    if (parseFloat(comprimento.value) = 0) {
-        alert("Campo 'Comprimento' vazio");
-    }
-    if (parseFloat(largura.value) = 0) {
-        alert("Campo 'Largura' vazio");
+    else if (parseFloat(document.getElementById("comprimento").value) <= 0) {
+        alert("Campo 'Comprimento' vazio ou Valor Inválido");
     }
 }
